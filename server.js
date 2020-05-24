e12d03e0980e31b303fde332129c7358435591bf
Komal Sainath <komal.sainath@proenco.com>
2020-05-24 19:35:10 +0530

    server for production build

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(favicon(path.join(__dirname , '/build/favicon.ico')));
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(port, () => {
+   console.log('Server is up!');
+});
